2c4f525d8022433fdd86ffb3c4d0d671f0331241
gukhyen <sosiq@kakao.com>
2022-12-27 18:52:55 +0900

    컴포넌트 props 사용

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,36 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function Header(){
+function Header(props: any){
   return <header className="App-header">
-    <h1><a href="/">React</a></h1>
+    <h1><a href="/">{props.title}</a></h1>
   </header>
 }
 
-function  Nav () {
-  return       <nav>
+function  Nav(props:any) {
+  const lis = [];
+
+  for(let i = 0; i < props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+  }
+
+  return <nav>
     <ol>
-      <li><a href="/read/1">html</a></li>
-      <li><a href="/read/2">css</a></li>
-      <li><a href="/read/3">js</a></li>
+      {lis}
     </ol>
   </nav>
 
 }
 
-function Article(){
+function Article(props:any){
   return     <article>
-    <h2>Welcome</h2>
-    Hello, WEB
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 
 function App() {
+  let topics: object;
+  topics = [
+    {id:1, title:'html'},
+    {id:2, title:'css'},
+    {id:3, title:'1'},
+    {id:4, title:'test'},
+  ];
+
   return (
     <div className="App">
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="ReactTest" />
+      <Nav topics={topics}></Nav>
+      <Article title="WelcomeTest" body="Hello, Body"></Article>
     </div>
   );
 }
