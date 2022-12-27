dbe86b5f0ae59baf786cd9e5bce8d958915bf642
gukhyen <sosiq@kakao.com>
2022-12-27 17:20:00 +0900

    - 배포판 생성 - 사용자 정의 태그 생성(컴포넌트)

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,23 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Header(){
+  return <header className="App-header">
+    <h1><a href="/">React</a></h1>
+  </header>
+}
+
+function  Nav () {
+  return       <nav>
+    <ol>
+      <li><a href="/read/1">html</a></li>
+      <li><a href="/read/2">css</a></li>
+      <li><a href="/read/3">js</a></li>
+    </ol>
+  </nav>
+
+}
+
+function Article(){
+  return     <article>
+    <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
